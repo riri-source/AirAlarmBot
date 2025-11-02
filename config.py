c24be2aa7c7e866e1c3144e3af8393546f53e9e5
Svitlana Orlova <nobody@nowhere.aha>
2025-11-02 20:05:17 +0200

    Moving ALERT_TYPES/REGIONS into config.py

--- config.py
+++ config.py
@@ -3,6 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ALERT_TYPES_UA = {
+    "air_raid": "Повітряна тривога!",
+    "chemical": "Хімічна тривога",
+    "radiation": "Радіаційна тривога",
+    "other": "Інша тривога",
+}
+
+KYIV_REGIONS = [
+    "Бучанський район", "Вишгородський район", "Фастівський район",
+    "Обухівський район", "Білоцерківський район", "Бориспільський район",
+    "Броварський район", "м. Київ"
+]
+
 API_URL = "https://api.alerts.in.ua/v1/alerts/active.json"
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ALERTS_TOKEN = os.getenv("ALERTS_TOKEN")
